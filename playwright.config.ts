4fb0ffd4964286a6cf1e74a9df931c7f530f27f6
Velena76 <medoshina@ya.ru>
2026-06-15 21:41:48 +0300

    allure init

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,11 @@ const config: PlaywrightTestConfig = {
             outputFolder: 'playwright-report', // папка (по умолчанию)
             open: 'never', // 'always' | 'never' | 'on-failure'
         }],
+        ['allure-playwright', {
+            detail: true, // шаги внутри теста
+            outputFolder: 'allure-results', // сырые JSON файлы
+            suiteTitle: false, // не дублировать describe в заголовке
+        }],
     ],
     globalTimeout: 1_000_000,
     use: {
